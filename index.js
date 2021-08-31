1282b4f3c9e6a6dad196d5aaf330b95416054c35
Noah Spencer <noahspencer0522@gmail.com>
2021-08-31 12:17:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if(feet <= 400) {
+    
+
+  } else if(feet > 1999 && feet < 2500) {
+
+  } else {
+
+  }
+
 }
 
 function ternaryCheckCity(){
